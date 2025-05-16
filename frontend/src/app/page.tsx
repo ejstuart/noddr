50fe41a5deaded6065533827a4413198f4fe7c6b
Eric Stuart <ejstuart@gmail.com>
2025-05-16 22:09:06 +1200

    Add prettier. Add a random test page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Let&apos;s add a new page {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
+              Goto /test
             </code>
             .
           </li>
@@ -39,7 +39,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            I love you Peleah! 
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
---
+++ frontend/src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css';
+
+export default function Test() {
+    return (
+        <div>
+            <h1 className={styles.heading}>This is a new page</h1>
+            <p className={styles.paragraph}>WUBBA WUBBA WUBBA</p>
+        </div>
+    )
+}
